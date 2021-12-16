215dc131c7ae93592bcb0604a1add3b0e3ec3a09
Michael FIG <mfig@agoric.com>
2021-12-16 10:40:57 -0600

    feat(options): ugly Agoric wallet options

--- source/options-storage.js
+++ source/options-storage.js
@@ -2,9 +2,7 @@ import OptionsSync from 'webext-options-sync';
 
 export default new OptionsSync({
 	defaults: {
-		colorRed: 244,
-		colorGreen: 67,
-		colorBlue: 54,
+		walletUrls: ['http://localhost:8000/wallet/', 'https://wallet.agoric.app/'],
 	},
 	migrations: [
 		OptionsSync.migrations.removeUnused,
--- source/options.js
+++ source/options.js
@@ -3,23 +3,69 @@ import browser from 'webextension-polyfill';
 
 import optionsStorage from './options-storage.js';
 
-optionsStorage.syncForm('#options-form');
+const awu = document.getElementById('agoric-wallet-urls');
+const newUrl = document.getElementById('new-wallet-url');
 
-const rangeInputs = [...document.querySelectorAll('input[type="range"][name^="color"]')];
-const numberInputs = [...document.querySelectorAll('input[type="number"][name^="color"]')];
-const output = document.querySelector('.color-output');
+let walletUrls = [];
+let defaultUrl;
 
-function updateColor() {
-	output.style.backgroundColor = `rgb(${rangeInputs[0].value}, ${rangeInputs[1].value}, ${rangeInputs[2].value})`;
+const makeListItem = (url, isDefault) => {
+  const li = document.createElement('li');
+  const input = document.createElement('input');
+  input.type = 'radio';
+  input.name = 'defaultWallet';
+  input.value = url;
+  if (isDefault) {
+    input.checked = true;
+  }
+  input.addEventListener('click', e => {
+    defaultUrl = url;
+    optionsStorage.set({ defaultUrl });
+  });
+  li.appendChild(input);
+  const link = document.createElement('a');
+  link.href = url;
+  link.target = '_blank';
+  link.textContent = url;
+  li.appendChild(link);
+
+  const button = document.createElement('button');
+  button.textContent = 'X';
+  button.addEventListener('click', async e => {
+    e.preventDefault();
+    e.stopPropagation();
+    walletUrls = walletUrls.filter(w => w !== url);
+    awu.removeChild(li);
+    if (defaultUrl === url) {
+      if (newUrl.nextElementSibling) {
+        const nextInput = newUrl.nextElementSibling.querySelector('input');
+        nextInput.checked = true;
+        defaultUrl = nextInput.value;
+      }
+    }
+    await optionsStorage.set({ walletUrls, defaultUrl });
+  });
+  li.appendChild(button);
+  return li;
+};
+
+async function init() {
+  ({walletUrls, defaultUrl} = await optionsStorage.getAll());
+  walletUrls.forEach(url => {
+    awu.appendChild(makeListItem(url, url === defaultUrl));
+  });
+
+  newUrl.querySelector('input').addEventListener('keydown', async e => {
+    if (e.key === 'Enter' || e.keyCode === 13) {
+      e.preventDefault();
+      const li = makeListItem(e.target.value, false);
+      awu.insertBefore(li, newUrl.nextSibling);
+      e.target.value = '';
+
+      walletUrls = [e.target.value, ...walletUrls];
+      await optionsStorage.set({walletUrls, defaultUrl});
+    }
+  });
 }
 
-function updateInputField(event) {
-	numberInputs[rangeInputs.indexOf(event.currentTarget)].value = event.currentTarget.value;
-}
-
-for (const input of rangeInputs) {
-	input.addEventListener('input', updateColor);
-	input.addEventListener('input', updateInputField);
-}
-
-window.addEventListener('load', updateColor);
+init();
