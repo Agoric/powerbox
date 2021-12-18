e9b1f72a1b728bdaa4156a68f06127c9621369c9
Michael FIG <mfig@agoric.com>
2021-12-17 23:17:29 -0600

    feat: inject `globalThis.agoricPowerbox` object before scripts run

--- source/content.js
+++ source/content.js
@@ -1,60 +1,44 @@
-/* global window, document, MessageChannel */
+/* global window, document */
 /* eslint-disable import/no-extraneous-dependencies */
 import browser from 'webextension-polyfill';
 
 import optionsStorage from './options-storage.js';
 
 import { makeConnect } from './connect.js';
-import { assertAgoricId, makeRefreshPetdata } from './petdata.js';
+import { makeRefreshPetdata } from './petdata.js';
 import { checkPrivileged, makeRefreshPrivileged } from './privs.js';
+import { createAgoricPowerboxInBrowser } from './powerbox.js';
+import { makeInjectScript } from './inject.js';
+import { makeBrowserMessageHandler } from './browser-messages.js';
 
-const send = (o, ...opts) => {
-  console.log('content', o);
-  window.postMessage(o, '*', ...opts);
-};
+const injectScript = makeInjectScript({ document });
+const send = (o, ...opts) => window.postMessage(o, '*', ...opts);
 const refreshPetdata = makeRefreshPetdata({ send, document });
 const refreshPrivileged = makeRefreshPrivileged({ send });
 const connect = makeConnect({ document, window });
 
-window.addEventListener('message', async ev => {
-  const obj = ev.data;
-  switch (obj.type) {
-    case 'AGORIC_POWERBOX_CONNECT': {
-      const { defaultUrl } = await optionsStorage.getAll();
-      const { port1, port2 } = new MessageChannel();
-      const url = new URL('/wallet-bridge.html', defaultUrl);
-      connect({ port: port1, url: url.href });
-      send({ type: 'AGORIC_POWERBOX_CONNECTING' }, [port2]);
-      break;
-    }
-    case 'AGORIC_POWERBOX_EXPAND_PETDATA': {
-      const { petdata = {}, powerboxUrls = [] } = await optionsStorage.getAll();
-      const privileged = checkPrivileged({
-        location: window.location,
-        powerboxUrls,
-      });
-      refreshPetdata({ privileged, petdata }, true);
-      refreshPrivileged({ privileged }, true);
-      break;
-    }
-    case 'AGORIC_POWERBOX_SET_PETDATA': {
-      const { petdata = {}, powerboxUrls = [] } = await optionsStorage.getAll();
-      if (checkPrivileged({ location: window.location, powerboxUrls })) {
-        const { id, petdata: rawPet } = obj;
-        const pet = {};
-        assertAgoricId(id);
-        Object.entries(rawPet).forEach(([k, v]) => {
-          assertAgoricId(k);
-          if (typeof v === 'string' && v) {
-            pet[k] = v;
-          }
-        });
-        petdata[id] = pet;
-        await optionsStorage.set({ petdata: { ...petdata } });
-      }
-      break;
-    }
-    default:
+// If the injection throws, refuse to listen to messages.
+try {
+  injectScript(
+    `globalThis.agoricPowerbox = (${createAgoricPowerboxInBrowser})(window);`,
+  );
+} catch (e) {
+  console.error('Failed to inject agoricPowerbox:', e);
+  throw e;
+}
+
+const browserMessageHandler = makeBrowserMessageHandler({
+  connect,
+  location: window.location,
+  refreshPetdata,
+  refreshPrivileged,
+  getOptions: () => optionsStorage.getAll(),
+  setOptions: options => optionsStorage.set(options),
+  send,
+});
+window.addEventListener('message', ev => {
+  if (ev.source === window) {
+    browserMessageHandler(ev.data);
   }
 });
 
